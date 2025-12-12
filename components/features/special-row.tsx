e0a60991af6635d80661e1833481b7d4b62a4576
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2025-12-12 10:18:10 +0800

    Update special-row.tsx
    Update special-row.tsx

--- components/features/special-row.tsx
+++ components/features/special-row.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import Drawng914 from "@/public/banners/drawing914.png"
+import Drawng1012 from "@/public/banners/drawing1012.png"
 
 export function SpecialRow() {
 	return (
@@ -8,7 +8,7 @@ export function SpecialRow() {
 		<div className="w-full flex flex-col mx-auto bg-green-400 my-4 py-4">
 			<div className="relative m-0 w-full">
 				<div className="absolute top-0 w-full h-[300px] bg-sky-100 m-auto">
-					<Image src={Drawng914} alt="Drawng912" fill /> 
+					<Image src={Drawng1012} alt="Drawng912" fill /> 
 				</div>
 			</div>
 			<div>
