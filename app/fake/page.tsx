990c06e2ea6e2f54b3ea8902d5cecd8bb633a566
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-02 23:07:20 +0800

    Add RootPage component with ProductList

---
+++ app/fake/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProductList from "@/components/ProductList"
+
+
+export default function RootPage() {
+  return (
+    <>
+        <ProductList />
+    </>
+  )
+}
